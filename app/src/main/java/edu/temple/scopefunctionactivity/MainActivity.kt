c3631f46a086b1637aa7df93ef5f83346b15e3ea
Roy Shimon <tup00268@temple.edu>
2024-09-30 15:54:35 -0400

    refactored all 3 functions, kept the same functionality.

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -27,15 +27,24 @@ class MainActivity : AppCompatActivity() {
     // Look at the final/return value and build the function "working backwards"
 
     // Return a list of random, sorted integers
-    private fun getTestDataArray() : List<Int> {
-        val testArray = MutableList(10){ Random.nextInt()}
+    private fun getTestDataArray() : List<Int> = MutableList(10) { Random.nextInt()}.apply { sort() }
+        /*val testArray = MutableList(10){ Random.nextInt()}
         testArray.sort()
         return testArray
-    }
+        */
+
 
     // Return true if average value in list is greater than median value, false otherwise
     private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
-        val avg = listOfNumbers.average()
+        return listOfNumbers.let { sortedList ->
+            val avg = sortedList.average()
+            val median = if(sortedList.size % 2 == 0)
+                (sortedList.sorted()[sortedList.size / 2] + sortedList[(sortedList.size - 1) / 2]) / 2
+            else
+                sortedList.sorted()[sortedList.size / 2]
+            avg < median
+        }
+        /*val avg = listOfNumbers.average()
         val sortedList = listOfNumbers.sorted()
         val median = if (sortedList.size % 2 == 0)
             (sortedList[sortedList.size / 2] + sortedList[(sortedList.size - 1) / 2]) / 2
@@ -43,11 +52,18 @@ class MainActivity : AppCompatActivity() {
             sortedList[sortedList.size / 2]
 
         return avg < median
+        */
+
     }
 
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
     private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
-        val textView: TextView
+       return (recycledView as? TextView?: TextView(context).apply{
+           setPadding(5,10,10,0)
+           textSize = 22f
+       }).apply { text = collection[position].toString() }
+
+        /* val textView: TextView
 
         if (recycledView != null) {
             textView = recycledView as TextView
@@ -60,6 +76,8 @@ class MainActivity : AppCompatActivity() {
         textView.text = collection[position].toString()
 
         return textView
+        */
+
     }
 
 }
